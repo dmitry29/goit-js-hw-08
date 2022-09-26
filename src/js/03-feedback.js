f4012b8a9291525054e6c76f40c6a812099ee146
dmitry29 <dmitry29push@gmail.com>
2022-09-26 16:54:02 +0300

    final 8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,49 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('.feedback-form textarea'),
+  email: document.querySelector('.feedback-form input'),
+};
+
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+populateFormData();
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormInput(e) { 
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+  evt.currentTarget.reset();  
+  reset(formData);
+  localStorage.removeItem(STORAGE_KEY);
+  
+}
+
+function populateFormData() {
+  const savedData = localStorage.getItem(STORAGE_KEY);
+
+  const parsedData = JSON.parse(savedData);
+
+  if (savedData) {
+    refs.textarea.value = parsedData.message || '';
+    refs.email.value = parsedData.email || '';
+  }
+}
+
+function reset(obj) {
+  for (const [key, value] of Object.entries(obj)) {
+    if (typeof value === 'object' && value !== null) reset(value);
+    else obj[key] = '';
+  }
+}
