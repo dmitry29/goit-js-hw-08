ed3a81586fc7457a079b50b1ff6687dd4becb5e0
dmitry29 <dmitry29push@gmail.com>
2022-09-26 16:58:19 +0300

    123

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
@@ -29,3 +30,34 @@ const lightbox = new SimpleLightbox('.gallery a', {
   captionDelay: 250,
   captionsData: 'alt',
 });
+=======
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+import { galleryItems } from './gallery-items';
+
+console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+galleryContainer.innerHTML = creatElements(galleryItems);
+
+function creatElements(gallery) {
+  const imageEl = gallery
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+</li>`
+    )
+    .join('');
+
+  return imageEl;
+}
+
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
+
+
+>>>>>>> Stashed changes
